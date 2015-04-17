f3495d7851e7cbd83e47d2a0b5501fdb30f0083a
Yellowhandsomefly <I279KenEmily22@gmail.com>
2015-04-17 15:02:33 +0800

    Mystack.java
    改寫Mystack class 而去繼承ArrayList

---
+++ Mystack.java
@@ -0,0 +1,36 @@
+class Mystack extends ArrayList<Object>{
+	
+	
+	public void push(String string){
+		
+		super.add(string);
+	}
+	
+	public boolean isEmpty(){
+		
+		return super.isEmpty();
+	}	
+
+	public Object peek(){
+
+		return super.get(getSize() - 1);
+	}	
+
+	public int getSize(){
+
+		return super.size();
+	}
+
+	public Object pop(){
+    		
+		Object o = super.get(getSize() - 1);
+    		super.remove(getSize() - 1);
+    		return o;
+	}
+
+	@Override
+	public String toString(){
+		
+		return "stack: " + super.toString();
+	}
+}
