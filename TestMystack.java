d79a17e4b8efdc3506771c9a9e439dfcd037d233
Yellowhandsomefly <I279KenEmily22@gmail.com>
2015-04-17 15:13:54 +0800

    最終版
    增加註解

--- TestMystack.java
+++ TestMystack.java
@@ -1,16 +1,27 @@
+/**
+ * Name: 黃俊翔
+ * ID: U10316025 
+ * Ex: 11.10
+ * Information: 
+ *		This is a class which shows the result of the string in reserve order.
+ */
 import java.util.Scanner;
 import java.util.ArrayList;
 
+/**Main method*/
 public class TestMystack{
 
 	public static void  main(String[] args){
 		
+		/**Create Scanner object*/
 		Scanner input = new Scanner(System.in);
-
+	
+		/**Create stack object*/
 		Mystack stack = new Mystack();
 
 		System.out.println("Enter five strings");
 
+		/**Input five string*/
 		for(int i = 1; i <= 5; i++){		
 	
 			System.out.println("Enter strings");
@@ -19,6 +30,7 @@ public class TestMystack{
 			
 		}
 
+		/**Print the string in reverse order*/
 		while(!stack.isEmpty()){
 
 			System.out.print(stack.pop() + " ");
