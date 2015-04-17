4a78a8a88c1e6d3d068c22bf274f07ceea7c8cf5
Yellowhandsomefly <I279KenEmily22@gmail.com>
2015-04-17 15:26:13 +0800

    最終版
    增加註解

--- Mystack.java
+++ Mystack.java
@@ -1,27 +1,31 @@
 import java.util.ArrayList;
 class Mystack extends ArrayList<Object>{
 	
-	
+	/**Add a new string to arrayList*/
 	public void push(String string){
 		
 		super.add(string);
 	}
 	
+	/**Return true if arrayList is empty*/
 	public boolean isEmpty(){
 		
 		return super.isEmpty();
 	}	
 
+	/**Return a top string*/
 	public Object peek(){
 
 		return super.get(getSize() - 1);
 	}	
 
+	/**Return the size of arrayList*/
 	public int getSize(){
 
 		return super.size();
 	}
 
+	/**Return a top string and delete the string*/
 	public Object pop(){
     		
 		Object o = super.get(getSize() - 1);
@@ -29,6 +33,7 @@ class Mystack extends ArrayList<Object>{
     		return o;
 	}
 
+	/**Return a string representation of all the elements*/
 	@Override
 	public String toString(){
 		
